a2c740956eb745064356678f21964ca1efa5aca5
nakatani-s <nakatani-s@roboken.iit.tsukuba.ac.jp>
2020-12-24 18:21:21 +0900

    first commit

---
+++ include/params.cuh
@@ -0,0 +1,35 @@
+/*
+define parameters for MCMPC and Systems
+*/
+
+#include <math.h>
+
+#ifndef PARAMS_CUH
+#define PARAMS_CUH
+
+#define TIME 1
+// #define dim_param 10
+#define dim_param 12
+#define dim_state 3
+#define dim_U 1
+
+#define dim_weight_matrix 4
+#define NUM_CONST 4
+#define interval 0.01
+#define Variavility 3.0
+
+#define Recalc 20
+#define N_OF_SAMPLES 3000
+#define THREAD_PER_BLOCKS 100
+#define HORIZON 15
+
+const int dim_hat_Q = HORIZON * HORIZON;
+
+const int CMA_mu = 9;
+const int c_sigma = 0.21;
+const int d_sigma = 5.76;
+const int Xi = 3.81;
+
+#define USING_THRUST
+
+#endif
